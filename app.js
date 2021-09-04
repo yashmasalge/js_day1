8f46ed269c8e4b1be7772af454d957ba4178688d
yashmasalge <myash2101@gmail.com>
2021-09-04 19:31:02 +0530

    classList

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // const element = document.getElementById("selectme")
-
 // const element = document.getElementsByClassName('heading')
-
 // const element = document.getElementsByTagName('h2')
-
 // const element = document.querySelectorAll('.heading')
 
-const element = document.querySelector('#selectme')
+ const element = document.querySelector('#selectme')
+// element.innerHTML = 'This is abcde'
+// element.style.color = 'cyan'
+// element.style = 'display : none'
 
-console.log(element)
+
+// console.log(element.classList)
+// element.classList.add('five')
+// console.log(element.classList)
+// element.classList.remove('one')
+// console.log(element.classList)
+
+// const body = document.querySelector('body')
+// console.log(body.classList.toggle('dark'))
+
+const value = element.classList.contains('one')
+
+console.log(value)
