19c661c8f121ae20fd67cc9e46361a02e4e86ba6
yashmasalge <myash2101@gmail.com>
2021-09-04 19:59:39 +0530

    event listeners intro

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // const element = document.getElementsByTagName('h2')
 // const element = document.querySelectorAll('.heading')
 
- const element = document.querySelector('#selectme')
+//  const element = document.querySelector('#selectme')
 // element.innerHTML = 'This is abcde'
 // element.style.color = 'cyan'
 // element.style = 'display : none'
@@ -18,6 +18,18 @@
 // const body = document.querySelector('body')
 // console.log(body.classList.toggle('dark'))
 
-const value = element.classList.contains('one')
+// const value = element.classList.contains('one')
+// console.log(value)
 
-console.log(value)
+
+// Button 
+
+const button = document.querySelector('button')
+const element = document.querySelector('#selectme')
+const element1 = document.querySelector('body')
+
+// console.log(button)
+
+button.addEventListener('click', () => {
+    element1.classList.toggle('dark')
+})
