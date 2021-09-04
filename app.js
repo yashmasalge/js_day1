c08de8d45bd3bfb05339e27a81cf3504e6b9fb29
yashmasalge <myash2101@gmail.com>
2021-09-04 18:59:35 +0530

    dom selectors

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const element = document.getElementById("selectme")
+// const element = document.getElementById("selectme")
+
+// const element = document.getElementsByClassName('heading')
+
+// const element = document.getElementsByTagName('h2')
+
+// const element = document.querySelectorAll('.heading')
+
+const element = document.querySelector('#selectme')
 
 console.log(element)
